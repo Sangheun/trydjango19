124cb7469714ddd0aed13e5e7da7fa8be08924d9
Lee Sangheun <heun0108@gmail.com>
2016-07-27 17:54:40 +0900

    SlugField

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,5 +1,9 @@
-from django.db import models
+
 from django.core.urlresolvers import reverse
+from django.db import models
+from django.db.models.signals import pre_save
+
+from django.utils.text import slugify
 # Create your models here.
 # model view controller
 
@@ -8,6 +12,7 @@ def upload_location(instance, filename):
 
 class Post(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField(unique=True)
     image = models.ImageField(upload_to=upload_location,
         null=True,
         blank=True,
@@ -24,8 +29,27 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("posts:detail", kwargs={"id": self.id})
+        return reverse("posts:detail", kwargs={"slug": self.slug})
         # return "/posts/%s/" %(self.id)
 
     class Meta:
         ordering = ["-timestamp", "-updated"]
+
+
+def create_slug(instance, new_slug=None):
+    slug = slugify(instance.title)
+    if new_slug is not None:
+        slug = new_slug
+    qs = Post.objects.filter(slug=slug).order_by("-id")
+    exists = qs.exists()
+    if exists:
+        new_slug = "%s-%s" %(slug, qs.first().id)
+        return create_slug(instance, new_slug=new_slug)
+    return slug
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
+
--- src/posts/views.py
+++ src/posts/views.py
@@ -24,9 +24,9 @@ def post_create(request):
     return render(request, 'post_form.html', context )
 
 
-def post_detail(request, id):
-    # instance = Post.objects.get(id=100)
-    instance = get_object_or_404(Post, id=id)
+def post_detail(request, slug):
+    # instance = Post.objects.get(slug=100)
+    instance = get_object_or_404(Post, slug=slug)
     context = {
         "title":instance.title,
         "instance": instance,
@@ -67,8 +67,8 @@ def post_list(request):
 
 
 
-def post_update(request, id=None):
-    instance = get_object_or_404(Post, id=id)
+def post_update(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
     form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
@@ -83,8 +83,8 @@ def post_update(request, id=None):
     return render(request, 'post_form.html', context )
 
 
-def post_delete(request, id=None):
-    instance = get_object_or_404(Post, id=id)
+def post_delete(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request, "Successfully Deleted")
     return redirect("posts:list")
